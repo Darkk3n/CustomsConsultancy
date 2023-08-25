850c1d2628ff7963bb55963d400b405c19915699
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-08-24 22:27:02 -0600

    Initial Commit

---
+++ CustomsConsultancy.Web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes } from 'react-router-dom'
+import './App.css'
+
+export const App = () => {
+  return (
+    <>
+      <h1>I.C. Aduanal</h1>
+      <Routes>
+
+      </Routes>
+    </>
+  )
+}

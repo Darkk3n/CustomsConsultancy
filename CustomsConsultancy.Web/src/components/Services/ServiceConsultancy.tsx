3973beb82947b9da600638df1ed6fa9025167566
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-09-10 21:57:57 -0600

    Added base text on Consultancy and Instruction

--- CustomsConsultancy.Web/src/components/Services/ServiceConsultancy.tsx
+++ CustomsConsultancy.Web/src/components/Services/ServiceConsultancy.tsx
@@ -1,6 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap"
 
 export const ServiceConsultancy = () => {
    return (
-      <div>ServiceConsultancy</div>
+      <Container>
+         <Row>
+            <Col md={6}></Col>
+            <Col md={6}>
+               <h1>Consultoría:</h1>
+               <p className="text-start">
+                  Proporcionamos asesoría jurídica relativa interpretación y aplicación de las Leyes y normatividad legal aplicable en la materia aduanal, que pueden ser de dos tipos:
+               </p>
+               <ul className="text-start">
+                  <li>Orientación Jurídica Aduanal</li>
+                  <li>Consultoría de Análisis Especial</li>
+               </ul>
+            </Col>
+         </Row>
+      </Container>
    )
 }
--- CustomsConsultancy.Web/src/components/Services/ServiceInstruction.tsx
+++ CustomsConsultancy.Web/src/components/Services/ServiceInstruction.tsx
@@ -1,6 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap"
 
 export const ServiceInstruction = () => {
    return (
-      <div>ServiceInstruction</div>
+      <Container style={{ color: 'black', backgroundSize: '100%', backgroundImage: 'url(src/assets/img/ContactUs.png)' }}>
+         <Row>
+            <Col md={6}>
+               <h1>Instrucción:</h1>
+               <p className="text-start">
+                  Impartimos cursos de capacitación de los cuales podrás obtener valor agregado con nuevos conocimientos, información, actualización e instrucción en temas relativos a la materia aduanal.
+               </p>
+            </Col>
+            <Col md={6}></Col>
+         </Row>
+      </Container>
    )
 }

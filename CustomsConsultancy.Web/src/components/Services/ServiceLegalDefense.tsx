e18468b5d901ede5fe0f22be1471959495cd45b3
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-09-14 17:25:09 -0600

    Added text to Legal Defense

--- CustomsConsultancy.Web/src/components/Services/ServiceLegalDefense.tsx
+++ CustomsConsultancy.Web/src/components/Services/ServiceLegalDefense.tsx
@@ -1,6 +1,20 @@
+import { Col, Container, Row } from "react-bootstrap"
 
 export const ServiceLegalDefense = () => {
    return (
-      <div>ServiceLegalDefense</div>
+      <Container>
+         <Row>
+            <Col md={6}>
+               <h1>Defensa Juridica</h1>
+               <p className="text-start">Se realiza el estudio de los actos de autoridad en particular y se define una estrategia legal que permita lograr el mejor beneficio de sus intereses.</p>
+               <ul className="text-start">
+                  <li>Procedimientos administrativos aduaneros</li>
+                  <li>Recursos de revocacion</li>
+                  <li>Juicios de nulidad</li>
+                  <li>Juicios de amparo</li>
+               </ul>
+            </Col>
+         </Row>
+      </Container>
    )
 }

df8beb281dc02b2e7dc14cd62d85b0a9adfaa0a4
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-09-14 18:41:48 -0600

    Added Customs Agency text

--- CustomsConsultancy.Web/src/components/Services/ServiceCustomsAgency.tsx
+++ CustomsConsultancy.Web/src/components/Services/ServiceCustomsAgency.tsx
@@ -1,6 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap"
 
 export const ServiceCustomsAgency = () => {
    return (
-      <div>ServiceCustomsAgency</div>
+      <Container>
+         <Row>
+            <Col md={6}></Col>
+            <Col md={6}>
+               <h1>Despacho Aduanal:</h1>
+               <p className="text-end">
+                  Proporcionamos asesoría jurídica y operativa en los trámites que se deben de realizar ante la Aduana de México en términos de la normatividad legal aplicable en materia aduanal.
+               </p>
+            </Col>
+         </Row>
+      </Container>
    )
 }

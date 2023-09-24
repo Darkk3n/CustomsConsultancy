64b7660d20c7a868095aa9b857fce319551f1595
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-09-23 21:15:11 -0600

    PrivacyAgreement fixes

--- CustomsConsultancy.Web/src/components/Contact/PrivacyAgreement.tsx
+++ CustomsConsultancy.Web/src/components/Contact/PrivacyAgreement.tsx
@@ -23,33 +23,31 @@ export const PrivacyAgreement = ({ showModal, hideModal }: Props) => {
             </p>
             <p>
                La utilización de los datos recopilados será utilizada exclusivamente para:
-               <ol type="I">
-                  <li>Proveer servicios requeridos, así como actividades afines</li>
-                  <li>Informar sobre nuevos servicios que estén relacionado con nuestra actividad</li>
-                  <li>Dar cumplimiento a obligaciones contraídas con nuestros clientes</li>
-                  <li>Evaluar la calidad del servicio</li>
-                  <li>Fines de identificación</li>
-                  <li>Fines estadísticos</li>
-                  <li>Para invitar a los integrantes de esta a los eventos y promociones realizadas por I.C. ADUANAL y
-                  </li>
-                  <li>Para eventualmente contactarlo vía correo electrónico con el fin de compartirle noticias de interés sobre aspectos aduaneros, de comercio exterior y de derecho en general.</li>
-               </ol>
             </p>
+            <ol type="I">
+               <li>Proveer servicios requeridos, así como actividades afines</li>
+               <li>Informar sobre nuevos servicios que estén relacionado con nuestra actividad</li>
+               <li>Dar cumplimiento a obligaciones contraídas con nuestros clientes</li>
+               <li>Evaluar la calidad del servicio</li>
+               <li>Fines de identificación</li>
+               <li>Fines estadísticos</li>
+               <li>Para invitar a los integrantes de esta a los eventos y promociones realizadas por I.C. ADUANAL y
+               </li>
+               <li>Para eventualmente contactarlo vía correo electrónico con el fin de compartirle noticias de interés sobre aspectos aduaneros, de comercio exterior y de derecho en general.</li>
+            </ol>
             <p>Quien desee ser dado de baja de nuestra base de datos podrá hacerlo por cualquiera de los siguientes medios:
             </p>
             <p>
                Enviar un correo electrónico a ic.aduanal@gmail.com con el asunto BAJA indicando la cuenta de correo que desee sea eliminada.
             </p>
             <p>
-               Contactarnos vía telefónica al 229 463 9014 de lunes a viernes y de 9 AM a 5 PM indicando los datos de la cuenta de correo electrónico que desean sea removida de la base de datos
+               Contactarnos vía telefónica al 229 463 9014 de Lunes a Viernes y de 9 AM a 5 PM indicando los datos de la cuenta de correo electrónico que desean sea removida de la base de datos
             </p>
-            <p>
-               <ol>
-                  <li>Usted tiene el derecho de acceder a sus datos personales que poseemos y a los detalles del tratamiento de estos, así como a rectificarlos en caso de ser inexactos o darlos de baja cuando considere que resulten ser excesivos o innecesarios para las finalidades que justificaron su obtención u oponerse al tratamiento de estos para fines específicos.</li>
-                  <li>Para limitar el uso o divulgación de sus datos personales, así como para ejercer los derechos de acceso, rectificación, cancelación u oposición, deberá presentar una solicitud al correo electrónico ic.aduanal@gmail.com</li>
-                  <li>El presente Aviso de Privacidad podrá ser modificado y actualizado en cualquier momento. I.C. ADUANAL notificará los cambios al Aviso de Privacidad mediante el correo electrónico ic.aduanal@gmail.com</li>
-               </ol>
-            </p>
+            <ol>
+               <li>Usted tiene el derecho de acceder a sus datos personales que poseemos y a los detalles del tratamiento de estos, así como a rectificarlos en caso de ser inexactos o darlos de baja cuando considere que resulten ser excesivos o innecesarios para las finalidades que justificaron su obtención u oponerse al tratamiento de estos para fines específicos.</li>
+               <li>Para limitar el uso o divulgación de sus datos personales, así como para ejercer los derechos de acceso, rectificación, cancelación u oposición, deberá presentar una solicitud al correo electrónico ic.aduanal@gmail.com</li>
+               <li>El presente Aviso de Privacidad podrá ser modificado y actualizado en cualquier momento. I.C. ADUANAL notificará los cambios al Aviso de Privacidad mediante el correo electrónico ic.aduanal@gmail.com</li>
+            </ol>
             <p>Responsable: Andrés Aguilar Sánchez y/o I.C. ADUANAL</p>
             <p>NO SE RECABA NINGÚN DATO PERSONAL SENSIBLE EN TÉRMINOS DE LA LEY. LOS DATOS QUE SE RECABAN SON NOMBRE DEL PARTICIPANTE, EMPRESA EN LA QUE TRABAJA, CORREO ELECTRÓNICO, DOMICILO FISCAL Y TELÉFONO</p>
          </Modal.Body>

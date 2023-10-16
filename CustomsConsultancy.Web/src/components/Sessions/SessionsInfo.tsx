0a3af3f73803ce41ebfbce8309ae0007e66d960c
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-10-15 19:42:33 -0600

    Fixed CSS

--- CustomsConsultancy.Web/src/components/Sessions/SessionsInfo.tsx
+++ CustomsConsultancy.Web/src/components/Sessions/SessionsInfo.tsx
@@ -3,17 +3,17 @@ import './SessionsInfo.css'
 export const SessionsInfo = () => {
    return (
       <div className="sessions-info-background d-flex align-items-start justify-content-center flex-column">
-         <div style={{ width: '60%', textAlign: 'justify' }} className="d-flex">
+         <div className="d-flex container-size">
             <p>
                En I.C. Aduanal estamos conscientes de la importancia y necesidad de contar con espacios donde se ponga a disposición de la generalidad SESIONES DE CAPACITACIÓN donde se aborde el estudio de los diversos temas que integran el Despacho Aduanero de Mercancías.
             </p>
          </div>
-         <div style={{ width: '60%', textAlign: 'justify' }} className='d-flex'>
+         <div className='d-flex container-size'>
             <p>
                En nuestra SESIONES DE ESTUDIO ADUANAL abordaremos en forma clara y sencilla el estudio de las diferentes figuras jurídicas que se integran en la Ley Aduanera y demás ordenamientos aplicables y que regulan la entrada y salida de mercancías de nuestro país, Mexico.
             </p>
          </div>
-         <div style={{ width: '60%', textAlign: 'justify' }} className='d-flex'>
+         <div className='d-flex container-size'>
             <p>
                Los temas que se abordaran en las SESIONES DE ESTUDIO ADUANAL son, por ejemplo:
             </p>

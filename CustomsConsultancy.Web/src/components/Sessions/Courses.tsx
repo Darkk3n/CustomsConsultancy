0e87768daba62c36c7a384457d1ffea508ee08c1
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-10-16 23:11:19 -0600

    2nd carousel option

--- CustomsConsultancy.Web/src/components/Sessions/Courses.tsx
+++ CustomsConsultancy.Web/src/components/Sessions/Courses.tsx
@@ -1,48 +1,21 @@
-import { Carousel } from 'react-bootstrap'
 import './Courses.css'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export const Courses = () => {
-
+   const images = [
+      "https://res.cloudinary.com/ifeomaimoh/image/upload/v1652345767/demo_image2.jpg",
+      "https://res.cloudinary.com/ifeomaimoh/image/upload/v1652366604/demo_image5.jpg",
+      "https://res.cloudinary.com/ifeomaimoh/image/upload/v1652345874/demo_image1.jpg",
+   ];
 
    return (
-      // <Carousel>
-      //    <Carousel.Item>
-      //       //Image here
-      //       <Carousel.Caption>
-      //          <h3>Curso 1</h3>
-      //       </Carousel.Caption>
-      //    </Carousel.Item>
-
-      //    <Carousel.Item>
-      //       //Image here
-      //       <Carousel.Caption>
-      //          <h3>Curso 2</h3>
-      //       </Carousel.Caption>
-      //    </Carousel.Item>
-      // </Carousel>
-      <Carousel>
-         <Carousel.Item>
-            <img
-               className="d-block w-100"
-               src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-               alt="Image One"
-            />
-            <Carousel.Caption>
-               <h3>Label for first slide</h3>
-               <p>Sample Text for Image One</p>
-            </Carousel.Caption>
-         </Carousel.Item>
-         <Carousel.Item>
-            <img
-               className="d-block w-100"
-               src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-               alt="Image Two"
-            />
-            <Carousel.Caption>
-               <h3>Label for second slide</h3>
-               <p>Sample Text for Image Two</p>
-            </Carousel.Caption>
-         </Carousel.Item>
+      <Carousel useKeyboardArrows={true}>
+         {images.map((URL, index) => (
+            <div className="slide">
+               <img alt="sample_file" src={URL} key={index} />
+            </div>
+         ))}
       </Carousel>
    )
 }

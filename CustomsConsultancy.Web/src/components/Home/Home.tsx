70dd6a9944fc24d945646fd31bb577dfc81ffbf5
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-11-08 18:57:16 -0600

    Fixed CSS class for font

--- CustomsConsultancy.Web/src/components/Home/Home.tsx
+++ CustomsConsultancy.Web/src/components/Home/Home.tsx
@@ -4,10 +4,10 @@ export const Home = () => {
    return (
       <div className='back'>
          <div className='home-text'>
-            <p className='global-text'>
+            <p className='global-font'>
                Expertos en ASESORIA <span className='blue-text'>Aduanal</span> y <span className='blue-text'>Capacitación</span>
             </p>
-            <p className='global-text'>
+            <p className='global-font'>
                Experticia Legal para <span className='blue-text'> Potenciar</span> tus Operaciones Aduaneras y <span className='blue-text'> Cumplir</span> con <span className='yellow-text'> Éxito</span>
             </p>
          </div>

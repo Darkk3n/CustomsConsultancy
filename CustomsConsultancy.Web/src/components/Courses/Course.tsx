391638a7b952d0c1447c0da5869a428f1fa6f64b
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-11-12 21:10:46 -0600

    Added image and video link

--- CustomsConsultancy.Web/src/components/Courses/Course.tsx
+++ CustomsConsultancy.Web/src/components/Courses/Course.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useMemo, useState } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import { NavLink, useParams } from 'react-router-dom';
 import { CourseData } from '../../Models/CourseData';
 import './Course.css';
 export const Course = () => {
+   const baseImg = '../src/assets/img/';
    const mockData: CourseData[] = useMemo(() => {
       return [
          {
@@ -11,28 +12,32 @@ export const Course = () => {
             title: 'Curso 1',
             price: 1000,
             duration: '2 horas',
-            objective: 'Texto del Objetivo del curso 1'
+            objective: 'Texto del Objetivo del curso 1',
+            image: 'Course1.webp'
          },
          {
             id: 1,
             title: 'Curso 2',
             price: 2000,
             duration: '1 hora',
-            objective: 'Texto del Objetivo del curso 2'
+            objective: 'Texto del Objetivo del curso 2',
+            image: 'Course2.webp'
          },
          {
             id: 2,
             title: 'Curso 3',
             price: 3500,
             duration: '3 horas',
-            objective: 'Texto del Objetivo del curso 3'
+            objective: 'Texto del Objetivo del curso 3',
+            image: 'Course3.webp'
          },
          {
             id: 3,
             title: 'Curso 3',
             price: 1200,
             duration: '1 hora',
-            objective: 'Texto del Objetivo del curso 3'
+            objective: 'Texto del Objetivo del curso 3',
+            image: 'Course4.webp'
          },
       ]
    }, [])
@@ -47,7 +52,10 @@ export const Course = () => {
    return (
       <>
          <Container>
-            <h1 className='regular-text' style={{ textAlign: 'justify' }}>{selectedCourse?.title}</h1>
+            <h1 className='regular-text'>{selectedCourse?.title}</h1>
+         </Container>
+         <Container>
+            <Image src={`${baseImg}${selectedCourse?.image}`} />
          </Container>
          <Container>
             <NavLink to='https://www.tiktok.com/@i.c.aduanal/video/7296225010857839878?_r=1&_t=8gyntQgfdXK' target='_blank' className='video-text'>
@@ -73,7 +81,7 @@ export const Course = () => {
                </Col>
                <Col md={4}>
                   <div className='flex-display'>
-                     <h4>Registro y Pago</h4>
+                     <h4 className='regular-text'>Registro y Pago</h4>
                      <Button variant='primary'>Registrarme al curso</Button>
                   </div>
                </Col>
@@ -82,10 +90,6 @@ export const Course = () => {
          <Container>
             <hr />
             <hr />
-         </Container>
-         <Container>
-            <h3 style={{ textAlign: 'justify' }}>Objetivo</h3>
-            <p style={{ textAlign: 'justify' }}>{selectedCourse?.objective}</p>
          </Container>
       </>
    )

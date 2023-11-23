7928609efeadeac0a9f4705979d1ca316893c719
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-23 13:21:10 -0600

    Changed mappings to be manual on PotentialClients

--- CustomsConsultancy.Admin.Api/Dtos/PotentialClientDto.cs
+++ CustomsConsultancy.Admin.Api/Dtos/PotentialClientDto.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CustomsConsultancy.Admin.Api.Models;
 
 namespace CustomsConsultancy.Admin.Api.Dtos
 {
@@ -12,5 +9,31 @@ namespace CustomsConsultancy.Admin.Api.Dtos
         public string Phone { get; set; }
         public string PotentialClientType { get; set; }
         public string TopicsOfInterest { get; set; }
+
+        public static PotentialClientDto ToDto(PotentialClient potentialClient)
+        {
+            return new PotentialClientDto
+            {
+                Name = potentialClient.Name,
+                Email = potentialClient.Email,
+                Phone = potentialClient.Phone,
+                PotentialClientType = potentialClient.PotentialClientType,
+                TopicsOfInterest = potentialClient.TopicsOfInterest
+            };
+        }
+
+        public static IEnumerable<PotentialClientDto> ToDtoList(IEnumerable<PotentialClient> potentialClients)
+        {
+            var toReturn = new List<PotentialClientDto>();
+            toReturn.AddRange(potentialClients.Select(r => new PotentialClientDto
+            {
+                Name = r.Name,
+                Email = r.Email,
+                Phone = r.Phone,
+                PotentialClientType = r.PotentialClientType,
+                TopicsOfInterest = r.TopicsOfInterest
+            }));
+            return toReturn;
+        }
     }
 }

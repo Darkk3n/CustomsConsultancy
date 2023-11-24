fd40718e61d290068436bfe4946e23585f683b22
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-24 10:47:29 -0600

    Added CourseMapper

--- CustomsConsultancy.Admin.Api/Dtos/CourseDto.cs
+++ CustomsConsultancy.Admin.Api/Dtos/CourseDto.cs
@@ -9,31 +9,5 @@ namespace CustomsConsultancy.Admin.Api.Dtos
         public string Duration { get; set; }
         public string FileName { get; set; }
         public string Status { get; set; }
-
-        public static CourseDto FromModel(Course course)
-        {
-            return new CourseDto
-            {
-                Title = course.Title,
-                Duration = course.Duration,
-                FileName = course.FileName,
-                Status = course.Status,
-                Price = course.Price
-            };
-        }
-
-        public static IEnumerable<CourseDto> FromModelList(IEnumerable<Course> courses)
-        {
-            var toReturn = new List<CourseDto>();
-            toReturn.AddRange(courses.Select(r => new CourseDto
-            {
-                Title = r.Title,
-                Duration = r.Duration,
-                FileName = r.FileName,
-                Price = r.Price,
-                Status = r.Status
-            }));
-            return toReturn;
-        }
     }
 }
--- CustomsConsultancy.Admin.Api/Models/Course.cs
+++ CustomsConsultancy.Admin.Api/Models/Course.cs
@@ -11,26 +11,5 @@ namespace CustomsConsultancy.Admin.Api.Models
         public string Status { get; set; }
         public string VideoId { get; set; }
         public List<Client> Clients { get; } = new();
-
-        public static Course FromDto(CourseDto dto)
-        {
-            return new Course
-            {
-                Title = dto.Title,
-                Duration = dto.Duration,
-                FileName = dto.FileName,
-                Price = dto.Price,
-                Status = dto.Status,
-            };
-        }
-
-        public void UpdateValues(CourseDto dto)
-        {
-            Title = dto.Title;
-            Price = dto.Price;
-            Duration = dto.Duration;
-            FileName = dto.FileName;
-            Status = dto.Status;
-        }
     }
 }

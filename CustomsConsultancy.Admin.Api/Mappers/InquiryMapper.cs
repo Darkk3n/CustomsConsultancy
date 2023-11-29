47a277c37fdce7c67e4df71e7dcf6fdf08bef5fc
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-29 15:37:57 -0600

    Added endpoint, mapper and dto

---
+++ CustomsConsultancy.Admin.Api/Mappers/InquiryMapper.cs
@@ -0,0 +1,38 @@
+using CustomsConsultancy.Admin.Api.Dtos;
+using CustomsConsultancy.Admin.Api.Models;
+
+namespace CustomsConsultancy.Admin.Api.Mappers
+{
+    public static class InquiryMapper
+    {
+        public static Inquiry FromDto(InquiryDto dto)
+        {
+            return new Inquiry
+            {
+                ClientName = dto.Name,
+                ClientLastName = dto.LastName,
+                Company = dto.Company,
+                Email = dto.Email,
+                Phone = dto.Phone,
+                MobilePhone = dto.MobilePhone,
+                Question = dto.Inquiry
+            };
+        }
+
+        public static IEnumerable<InquiryDto> ToDtoList(IEnumerable<Inquiry> inquiries)
+        {
+            var toReturn = new List<InquiryDto>();
+            toReturn.AddRange(inquiries.Select(i => new InquiryDto
+            {
+                Name = i.ClientName,
+                LastName = i.ClientLastName,
+                Company = i.Company,
+                Email = i.Email,
+                Phone = i.Phone,
+                Inquiry = i.Question,
+                MobilePhone = i.MobilePhone
+            }));
+            return toReturn;
+        }
+    }
+}

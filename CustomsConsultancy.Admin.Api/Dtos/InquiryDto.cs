8c965c18f26a443993e0c53e489be904626a6b32
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-29 15:37:57 -0600

    Added endpoint, mapper and dto

---
+++ CustomsConsultancy.Admin.Api/Dtos/InquiryDto.cs
@@ -0,0 +1,13 @@
+namespace CustomsConsultancy.Admin.Api.Dtos
+{
+    public class InquiryDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string MobilePhone { get; set; }
+        public string Inquiry { get; set; }
+    }
+}
---
+++ CustomsConsultancy.Admin.Api/Endpoints/InquiriesEndpoints.cs
@@ -0,0 +1,26 @@
+using CustomsConsultancy.Admin.Api.Dtos;
+using CustomsConsultancy.Admin.Api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomsConsultancy.Admin.Api.Endpoints
+{
+    public static class InquiriesEndpoints
+    {
+        public static void AddInquiriesEndpoints(this WebApplication app)
+        {
+            app.MapPost("/api/inquiries", async (ConsultancyContext context, [FromBody] InquiryDto dto) =>
+            {
+                var inquiry = InquiryMapper.FromDto(dto);
+                context.Inquiry.Add(inquiry);
+                await context.SaveChangesAsync();
+            });
+
+            app.MapGet("/api/inquiries", async (ConsultancyContext context) =>
+            {
+                var inquiries = await context.Inquiry.ToListAsync();
+                return Results.Ok(InquiryMapper.ToDtoList(inquiries));
+            });
+        }
+    }
+}

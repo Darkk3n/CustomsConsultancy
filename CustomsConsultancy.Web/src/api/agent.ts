f83a65079984bf67dbd08483eb38428cbfe7a7b2
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-11-30 19:34:45 -0600

    Added API agent

--- CustomsConsultancy.Web/src/api/agent.ts
+++ CustomsConsultancy.Web/src/api/agent.ts
@@ -1,29 +1,29 @@
 import axios, { AxiosResponse } from "axios";
 import { InquiryModel } from "../Models/InquiryModel";
-import { PotentialClientModel } from '../Models/PotentialClientModel';
+import { PotentialClientModel } from "../Models/PotentialClientModel";
 
-axios.defaults.baseURL = 'https://localhost:7108/api'
+axios.defaults.baseURL = "https://localhost:7108/api";
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body: object) => axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body: object) => axios.put<T>(url, body).then(responseBody),
-    del: <T>(url: string, body: object) => axios.delete<T>(url, body).then(responseBody),
-}
+	get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+	post: <T>(url: string, body: object) => axios.post<T>(url, body).then(responseBody),
+	put: <T>(url: string, body: object) => axios.put<T>(url, body).then(responseBody),
+	del: <T>(url: string, body: object) => axios.delete<T>(url, body).then(responseBody),
+};
 
 const Inquiries = {
-    create: (inquiry: InquiryModel) => requests.post<void>('/inquiries', inquiry),
-}
+	create: (inquiry: InquiryModel) => requests.post<void>("/inquiries", inquiry),
+};
 
 const PotentialClients = {
-    create: (data: PotentialClientModel) => requests.post<void>('/PotentialClient', data)
-}
+	create: (data: PotentialClientModel) => requests.post<void>("/PotentialClient", data),
+};
 
 const http = {
-    Inquiries,
-    PotentialClients
-}
+	Inquiries,
+	PotentialClients,
+};
 
 export default http;

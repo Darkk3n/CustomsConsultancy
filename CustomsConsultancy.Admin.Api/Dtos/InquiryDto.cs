02f950a33c26f1919524881b52ec54f2dc027449
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-12-01 17:17:04 -0600

    Created a propert Dto

--- CustomsConsultancy.Admin.Api/Dtos/InquiryDto.cs
+++ CustomsConsultancy.Admin.Api/Dtos/InquiryDto.cs
@@ -32,4 +32,15 @@ namespace CustomsConsultancy.Admin.Api.Dtos
     {
         public string Response { get; set; }
     }
+
+    public class InquiryCreateDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string MobilePhone { get; set; }
+        public string Inquiry { get; set; }
+    }
 }

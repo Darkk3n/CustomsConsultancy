873f39ca0be129f86aaa9ec3d9c8b77b5e444c8a
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-12-01 16:59:21 -0600

    Applied first changes by prettier

--- CustomsConsultancy.Admin.Web/src/components/Inquiries/InquiryList.tsx
+++ CustomsConsultancy.Admin.Web/src/components/Inquiries/InquiryList.tsx
@@ -3,34 +3,34 @@ import { Link } from 'react-router-dom'
 import { InquiryModel } from '../../../../CustomsConsultancy.Web/src/Models/InquiryModel'
 
 interface IInquiryListProps {
-   inquiries: InquiryModel[]
+	inquiries: InquiryModel[]
 }
 
 export const InquiryList = ({ inquiries }: IInquiryListProps) => {
-   return (
-      <Table bordered responsive="md">
-         <thead>
-            <tr>
-               <th>#</th>
-               <th>Nombre</th>
-               <th>Resumen</th>
-               <th></th>
-            </tr>
-         </thead>
-         <tbody>
-            {
-               inquiries.map((r: InquiryModel, index) => {
-                  return (<tr key={index}>
-                     <td>{r.inquiryId}</td>
-                     <td>{`${r.name} ${r.lastName}`}</td>
-                     <td>{r.inquiry}</td>
-                     <td>
-                        <Link to={`/inquiry/${r.inquiryId}`}>Ver detalles</Link>
-                     </td>
-                  </tr>)
-               })
-            }
-         </tbody>
-      </Table>
-   )
+	return (
+		<Table bordered responsive="md">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nombre</th>
+					<th>Resumen</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{
+					inquiries.map((r: InquiryModel, index) => {
+						return (<tr key={index}>
+							<td>{r.inquiryId}</td>
+							<td>{`${r.name} ${r.lastName}`}</td>
+							<td>{r.inquiry}</td>
+							<td>
+								<Link to={`/inquiry/${r.inquiryId}`}>Ver detalles</Link>
+							</td>
+						</tr>)
+					})
+				}
+			</tbody>
+		</Table>
+	)
 }

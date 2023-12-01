fd7c68704133731a4078298003538fef62fa2e43
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-11-30 19:37:57 -0600

    Added axios in admin project

---
+++ CustomsConsultancy.Admin.Web/src/api/agent.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from "axios";
+import { CourseData } from '../../../CustomsConsultancy.Web/src/Models/CourseData';
+
+axios.defaults.baseURL = 'https://localhost:7108/api'
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const requests = {
+   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+   post: <T>(url: string, body: object) => axios.post<T>(url, body).then(responseBody),
+   put: <T>(url: string, body: object) => axios.put<T>(url, body).then(responseBody),
+   del: <T>(url: string, body: object) => axios.delete<T>(url, body).then(responseBody),
+}
+
+const Courses = {
+   create: (course: CourseData) => requests.post<void>('/courses', course),
+   update: (course: CourseData) => requests.put<CourseData>(`/courses/${course.id}`, course),
+   delete: (courseId: number) => requests.del<void>(`/courses/${courseId}`, {}),
+   list: () => requests.get<CourseData[]>('/courses'),
+   getById: (courseId: number) => requests.get<CourseData>(`/course/${courseId}`)
+}
+
+const http = {
+   Courses
+}
+
+
+export default http;

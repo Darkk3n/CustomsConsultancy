f1181602b905dcec45663dc0d463c61e612eea10
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-11-30 19:34:45 -0600

    Added API agent

--- CustomsConsultancy.Web/src/api/agent.ts
+++ CustomsConsultancy.Web/src/api/agent.ts
@@ -1,8 +1,38 @@
-const baseUrl = 'https://localhost:7108/api'
+import axios, { AxiosResponse } from "axios";
+import { InquiryModel } from "../Models/InquiryModel";
+import { PotentialClientModel } from '../Models/PotentialClientModel';
 
-export function http(url: string, body: any) {
-    return fetch(`${baseUrl}${url}`, {
-        body: JSON.stringify(body)
-    })
-        .then(response => response.json())
+axios.defaults.baseURL = 'https://localhost:7108/api'
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const requests = {
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: object) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: object) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string, body: object) => axios.delete<T>(url, body).then(responseBody),
 }
+
+const Inquiries = {
+    create: (inquiry: InquiryModel) => requests.post<void>('/inquiries', inquiry),
+}
+
+// const Courses = {
+//     create: (course: CourseData) => requests.post<void>('/courses', course),
+//     update: (course: CourseData) => requests.put<CourseData>(`/courses/${course.id}`, course),
+//     delete: (courseId: number) => requests.del<void>(`/courses/${courseId}`, {}),
+//     list: () => requests.get<CourseData[]>('/courses'),
+//     getById: (courseId: number) => requests.get<CourseData>(`/course/${courseId}`)
+// }
+
+const PotentialClients = {
+    create: (data: PotentialClientModel) => requests.post<void>('/PotentialClient', data)
+}
+
+const http = {
+    Inquiries,
+    PotentialClients
+}
+
+
+export default http;

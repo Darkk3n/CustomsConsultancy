9d523d590ef4458c80ec0fbe37f772855685782b
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-12-01 21:49:54 -0600

    Added reset

--- CustomsConsultancy.Web/src/components/Contact/ContactForm.tsx
+++ CustomsConsultancy.Web/src/components/Contact/ContactForm.tsx
@@ -8,60 +8,63 @@ import { PhoneInput } from '../Inputs/PhoneInput';
 import './ContactForm.css';
 
 export const ContactForm = () => {
-   const { register, handleSubmit, control } = useForm<InquiryModel>();
+	const { register, handleSubmit, control, reset } = useForm<InquiryModel>();
 
-   const onSubmit = (values: InquiryModel) => {
-      console.log(values)
-      http.Inquiries.create(values)
-         .then(() => toast.success('Pregunta enviada con exito.'))
-   }
+	const onSubmit = (values: InquiryModel) => {
+		console.log(values)
+		http.Inquiries.create(values)
+			.then(() => {
+				toast.success('Pregunta enviada con exito.')
+				reset();
+			});
+	}
 
-   const { acceptPolicyElement } = useAcceptPolicy();
+	const { acceptPolicyElement } = useAcceptPolicy();
 
-   return (
-      <>
-         <Row>
-            <p style={{ marginTop: '20px', fontFamily: 'Bebas Neue', fontSize: '3rem' }}>
-               ¿Necesitas asesorías? Ponte en contacto
-            </p>
-         </Row>
-         <Container className='container-contact'>
-            <form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
-               <Row>
-                  <Col md={6}>
-                     <input required className='w-100 mt-1 mb-1 p-1' placeholder='Nombre' {...register('name')} />
-                     {/* <p>{errors.name?.message}</p> */}
-                  </Col>
-                  <Col md={6}>
-                     <input required className='w-100 mt-1 mb-1 p-1' placeholder='Apellido(s)' {...register('lastName')} />
-                  </Col>
-               </Row >
-               <Row>
-                  <Col md={6}>
-                     <input required className='w-100 mt-1 mb-1 p-1' placeholder='Empresa o Razón Social' {...register('company')} />
-                  </Col>
-                  <Col md={6}>
-                     <input className='w-100 mt-1 mb-1 p-1' placeholder='Correo Electronico' {...register('email')} />
-                  </Col>
-               </Row>
-               <Row>
-                  <Col md={6}>
-                     <PhoneInput fieldName="phone" placeholder="Telefono" control={control} />
-                     {/* <p>{errors.phone?.message}</p> */}
-                  </Col>
-                  <Col md={6}>
-                     <PhoneInput fieldName="mobilePhone" placeholder="Whatsapp" control={control} />
-                  </Col>
-               </Row>
-               <Row>
-                  <Col md={12}>
-                     <textarea style={{ resize: 'none' }} rows={5} className='w-100 mt-1 mb-1 p-1' placeholder='Mensaje' {...register('inquiry')} />
-                  </Col>
-               </Row>
-               <br />
-               {acceptPolicyElement}
-            </form>
-         </Container >
-      </>
-   )
+	return (
+		<>
+			<Row>
+				<p style={{ marginTop: '20px', fontFamily: 'Bebas Neue', fontSize: '3rem' }}>
+					¿Necesitas asesorías? Ponte en contacto
+				</p>
+			</Row>
+			<Container className='container-contact'>
+				<form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
+					<Row>
+						<Col md={6}>
+							<input required className='w-100 mt-1 mb-1 p-1' placeholder='Nombre' {...register('name')} />
+							{/* <p>{errors.name?.message}</p> */}
+						</Col>
+						<Col md={6}>
+							<input required className='w-100 mt-1 mb-1 p-1' placeholder='Apellido(s)' {...register('lastName')} />
+						</Col>
+					</Row >
+					<Row>
+						<Col md={6}>
+							<input required className='w-100 mt-1 mb-1 p-1' placeholder='Empresa o Razón Social' {...register('company')} />
+						</Col>
+						<Col md={6}>
+							<input className='w-100 mt-1 mb-1 p-1' placeholder='Correo Electronico' {...register('email')} />
+						</Col>
+					</Row>
+					<Row>
+						<Col md={6}>
+							<PhoneInput fieldName="phone" placeholder="Telefono" control={control} />
+							{/* <p>{errors.phone?.message}</p> */}
+						</Col>
+						<Col md={6}>
+							<PhoneInput fieldName="mobilePhone" placeholder="Whatsapp" control={control} />
+						</Col>
+					</Row>
+					<Row>
+						<Col md={12}>
+							<textarea style={{ resize: 'none' }} rows={5} className='w-100 mt-1 mb-1 p-1' placeholder='Mensaje' {...register('inquiry')} />
+						</Col>
+					</Row>
+					<br />
+					{acceptPolicyElement}
+				</form>
+			</Container >
+		</>
+	)
 }

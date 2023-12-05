66c7f54a913415743e774c2faa396a1a00ba8b1a
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-12-04 18:49:03 -0600

    Fixed image path

--- CustomsConsultancy.Admin.Web/src/components/NavBar.tsx
+++ CustomsConsultancy.Admin.Web/src/components/NavBar.tsx
@@ -1,25 +1,25 @@
-import { Navbar, Image, Nav } from 'react-bootstrap'
+import { Image, Nav, Navbar } from 'react-bootstrap'
 import './NavBar.css'
 
 export const NavBar = () => {
-    return (
-        <>
-            <Navbar expand="lg" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '10px' }}>
-                <Navbar.Brand href='/'>
-                    <Image src='src/assets/img/Logo.jpeg' alt='Logo' style={{ borderRadius: '50%', width: '10rem', paddingRight: '10px' }} />
-                </Navbar.Brand>
-                <Nav style={{ display: 'flex' }}>
-                    <Nav.Item>
-                        <Nav.Link href="/inquiries" className='menu-link'>Preguntas</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/courses" className='menu-link'>Cursos</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/potentialclients" className='menu-link'>Clientes Potenciales</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Navbar>
-        </>
-    )
+	return (
+		<>
+			<Navbar expand="lg" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '10px' }}>
+				<Navbar.Brand href='/'>
+					<Image src='../src/assets/img/Logo.jpeg' alt='Logo' style={{ borderRadius: '50%', width: '10rem', paddingRight: '10px' }} />
+				</Navbar.Brand>
+				<Nav style={{ display: 'flex' }}>
+					<Nav.Item>
+						<Nav.Link href="/inquiries" className='menu-link'>Preguntas</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link href="/courses" className='menu-link'>Cursos</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link href="/potentialclients" className='menu-link'>Clientes Potenciales</Nav.Link>
+					</Nav.Item>
+				</Nav>
+			</Navbar>
+		</>
+	)
 }

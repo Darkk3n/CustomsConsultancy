87502a4fa53bc4730d945a55a494c067ed488882
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-12-14 11:29:23 -0600

    Added Toaster

--- CustomsConsultancy.Admin.Web/src/main.tsx
+++ CustomsConsultancy.Admin.Web/src/main.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+	<BrowserRouter>
+		<Toaster richColors position='top-right' />
+		<App />
+	</BrowserRouter>
 )

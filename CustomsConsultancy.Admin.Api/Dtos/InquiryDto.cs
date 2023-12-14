8f612c266174326c9a04ddc543c0b93d766478e4
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-12-14 10:15:34 -0600

    Fixes after merge

--- CustomsConsultancy.Admin.Api/Dtos/InquiryDto.cs
+++ CustomsConsultancy.Admin.Api/Dtos/InquiryDto.cs
@@ -15,7 +15,6 @@ namespace CustomsConsultancy.Admin.Api.Dtos
 
     public class InquiryCreateDto
     {
-        public int InquiryId { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Company { get; set; }
@@ -23,24 +22,10 @@ namespace CustomsConsultancy.Admin.Api.Dtos
         public string Phone { get; set; }
         public string MobilePhone { get; set; }
         public string Inquiry { get; set; }
-        public bool Answered { get; set; }
     }
-
-
 
     public class InquiryAnswerDto
     {
         public string Response { get; set; }
-    }
-
-    public class InquiryCreateDto
-    {
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public string Company { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string MobilePhone { get; set; }
-        public string Inquiry { get; set; }
     }
 }

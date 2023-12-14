64a283d4f2fbbfceee9f73781853b205eda474b3
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-12-14 11:15:28 -0600

    File rename

--- CustomsConsultancy.Admin.Web/src/components/Inquiries/InquiriesIndex.tsx
+++ CustomsConsultancy.Admin.Web/src/components/Inquiries/InquiriesIndex.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Container, Tab, Tabs } from 'react-bootstrap';
 import { InquiryModel } from '../../../../CustomsConsultancy.Web/src/Models/InquiryModel';
-import http from '../../api/agent';
+import http from '../../api/adminAgent';
 import { InquiryList } from './InquiryList';
 
 export const InquiriesIndex = () => {

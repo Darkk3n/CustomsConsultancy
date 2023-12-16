4e343915eb2eb28d6f8a64edd0d2957172228c90
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-12-15 19:23:11 -0600

    Added default value on PhoneInput

--- CustomsConsultancy.Web/src/components/Inputs/PhoneInput.tsx
+++ CustomsConsultancy.Web/src/components/Inputs/PhoneInput.tsx
@@ -2,27 +2,28 @@ import { Controller } from 'react-hook-form';
 import { PatternFormat } from 'react-number-format';
 
 interface IProps {
-   fieldName: string;
-   placeholder: string;
-   control: any;
+	fieldName: string;
+	placeholder: string;
+	control: any;
 }
 
 export const PhoneInput = ({ fieldName, placeholder, control }: IProps) => {
-   return (
-      <Controller
-         render={({ field: { onChange, name, value } }) => (
-            <PatternFormat
-               className='w-100 mt-1 mb-1 p-1'
-               type='tel'
-               format={'###-###-####'}
-               placeholder={placeholder}
-               mask='_'
-               onChange={onChange}
-               name={name}
-               value={value} />
-         )}
-         name={fieldName}
-         control={control}
-      />
-   )
+	return (
+		<Controller
+			render={({ field: { onChange, name, value } }) => (
+				<PatternFormat
+					className='w-100 mt-1 mb-1 p-1'
+					type='tel'
+					format={'###-###-####'}
+					placeholder={placeholder}
+					mask='_'
+					onChange={onChange}
+					name={name}
+					value={value} />
+			)}
+			name={fieldName}
+			control={control}
+			defaultValue=''
+		/>
+	)
 }

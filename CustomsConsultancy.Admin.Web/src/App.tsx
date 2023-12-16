8b4b0aa7415d111e92fff05921c3bd5fa0e0a6ba
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-12-15 18:58:38 -0600

    Added home component and record count

--- CustomsConsultancy.Admin.Web/src/App.tsx
+++ CustomsConsultancy.Admin.Web/src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './Home';
 import { InquiriesIndex, InquiryDetails } from './components/Inquiries';
+import { NavBar } from './components/NavBar';
 
 function App() {
 	return (
 		<>
 			<NavBar />
 			<Routes>
-				<Route path='/' element={<div>Home</div>} />
+				<Route path='/' element={<Home />} />
 				<Route path='/inquiries' element={<InquiriesIndex />} />
 				<Route path='/courses' element={<div>Cursos</div>} />
 				<Route path='/potentialclients' element={<div>Clientes Potenciales</div>} />

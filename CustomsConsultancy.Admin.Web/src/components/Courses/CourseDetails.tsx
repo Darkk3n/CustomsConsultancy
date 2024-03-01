446ecdd4130c8b8944d83b9c290e1423b0dd80d8
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2024-02-29 19:44:56 -0600

    Added base UI

--- CustomsConsultancy.Admin.Web/src/components/Courses/CourseDetails.tsx
+++ CustomsConsultancy.Admin.Web/src/components/Courses/CourseDetails.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import http from "../../api/adminAgent";
 import { CourseModel } from '../../models/Courses/CourseIndexModel';
@@ -8,15 +10,55 @@ export const CourseDetails = () => {
 	const numCourseId = useMemo(() => Number(courseId), [courseId]);
 	const [course, setCourse] = useState<CourseModel>()
 
+	const newCourse = numCourseId === 0;
+
 	useEffect(() => {
 		http.Courses
 			.getById(numCourseId)
 			.then(r => setCourse(r))
 	}, [numCourseId])
 
+	const { handleSubmit, register } = useForm<CourseModel>()
 
+	const onSubmit = (data: CourseModel) => { }
 
 	return (
-		<div>Course Details: {course?.title}</div>
+		<Container>
+			{newCourse
+				? <h1>Nuevo Curso</h1>
+				: <h1>Editar Curso: {course?.title}</h1>
+			}
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Row>
+					<Col md={6}>
+						<input className="w-100 mb-3" placeholder="Nombre" {...register("title")} />
+					</Col>
+					<Col md={6}>
+						<input className="w-100 mb-3" placeholder="Duracion" {...register("duration")} />
+					</Col>
+				</Row>
+				<Row>
+					<Col md={6}>
+						<input className="w-100 mb-3" placeholder="Precio" {...register("price")} />
+					</Col>
+					<Col md={6}>
+						<input className="w-100 mb-3" placeholder="Nombre de Archivo" {...register("fileName")} />
+					</Col>
+				</Row>
+				<Row>
+					<Col md={6}>
+						<input className="w-100 mb-3" placeholder="Estatus" {...register("status")} />
+					</Col>
+					<Col md={6}>
+						<input className="w-100 mb-3" placeholder="ID Video TikTok" {...register("videoId")} />
+					</Col>
+				</Row>
+				<Row>
+					<Col md={12}>
+						<Button type="submit" variant="primary">Guardar</Button>
+					</Col>
+				</Row>
+			</form>
+		</Container>
 	)
 }

769922fea69d6b3ec28af438fa8d50e0c2f95443
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2024-03-20 20:07:53 -0600

    Created endpoint class and registration

---
+++ CustomsConsultancy.Admin.Api/Endpoints/CourseRegistrationEndpoints.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomsConsultancy.Admin.Api.Endpoints
+{
+    public static class CourseRegistrationEndpoints
+    {
+        public static void AddCourseRegistrationEndpoints(this WebApplication app)
+        {
+
+        }
+    }
+}

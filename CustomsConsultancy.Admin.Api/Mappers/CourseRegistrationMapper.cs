66d99165f6d01f4e2c04540c870b078efba5797b
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2024-03-20 20:18:20 -0600

    Added first endpoint, dto and mapper

---
+++ CustomsConsultancy.Admin.Api/Mappers/CourseRegistrationMapper.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomsConsultancy.Admin.Api.Dtos;
+using CustomsConsultancy.Admin.Api.Models;
+
+namespace CustomsConsultancy.Admin.Api.Mappers
+{
+    public static class CourseRegistrationMapper
+    {
+        public static List<CourseRegistrationDto> ToDtoList(List<CourseClients> registrations)
+        {
+            var toReturn = new List<CourseRegistrationDto>();
+            toReturn.AddRange(registrations.Select(r => new CourseRegistrationDto
+            {
+                ClientId = r.ClientId,
+                CourseId = r.CourseId
+            }));
+            return toReturn;
+        }
+    }
+}

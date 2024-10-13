fb7bf68567773f8daae3476b2239be27b928364c
Gerardo Aguilar <455198@petco.com>
2024-10-13 16:59:58 -0600

    Added last name field to Client

---
+++ CustomsConsultancy.Admin.Api/Migrations/20241013225915_AddLastNameToClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomsConsultancy.Admin.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastNameToClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Clients",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Clients");
+        }
+    }
+}
--- CustomsConsultancy.Admin.Api/Models/Client.cs
+++ CustomsConsultancy.Admin.Api/Models/Client.cs
@@ -3,9 +3,10 @@ namespace CustomsConsultancy.Admin.Api.Models
     public class Client : BaseEntity
     {
         public string Name { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
 
-        public List<Course> Courses { get; } = new();
+        public List<Course> Courses { get; } = [];
     }
 }

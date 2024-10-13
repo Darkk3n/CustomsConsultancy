902db786cbdc1f6d43dfb7cc476a23a83e4e5bbd
Gerardo Aguilar <455198@petco.com>
2024-10-13 14:05:07 -0600

    Added necessary fields to complete course registration

---
+++ CustomsConsultancy.Admin.Api/Migrations/20241013200338_AddFieldsToCourseSubscription.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomsConsultancy.Admin.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldsToCourseSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethod",
+                table: "CourseClients",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonOrCompanyName",
+                table: "CourseClients",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "CourseClients",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresInvoice",
+                table: "CourseClients",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rfc",
+                table: "CourseClients",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxPayerEmail",
+                table: "CourseClients",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TaxRegime",
+                table: "CourseClients",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "CourseClients");
+
+            migrationBuilder.DropColumn(
+                name: "PersonOrCompanyName",
+                table: "CourseClients");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "CourseClients");
+
+            migrationBuilder.DropColumn(
+                name: "RequiresInvoice",
+                table: "CourseClients");
+
+            migrationBuilder.DropColumn(
+                name: "Rfc",
+                table: "CourseClients");
+
+            migrationBuilder.DropColumn(
+                name: "TaxPayerEmail",
+                table: "CourseClients");
+
+            migrationBuilder.DropColumn(
+                name: "TaxRegime",
+                table: "CourseClients");
+        }
+    }
+}
--- CustomsConsultancy.Admin.Api/Models/CourseClients.cs
+++ CustomsConsultancy.Admin.Api/Models/CourseClients.cs
@@ -4,5 +4,16 @@ namespace CustomsConsultancy.Admin.Api.Models
     {
         public int CourseId { get; set; }
         public int ClientId { get; set; }
+
+        public bool RequiresInvoice { get; set; }
+
+        //If requires invoice is true, then the following fields are required
+        public string Rfc { get; set; }
+        public string PersonOrCompanyName { get; set; }
+        public string PostalCode { get; set; }
+        public int? TaxRegime { get; set; }
+        public string TaxPayerEmail { get; set; }
+
+        public int PaymentMethod { get; set; }
     }
 }

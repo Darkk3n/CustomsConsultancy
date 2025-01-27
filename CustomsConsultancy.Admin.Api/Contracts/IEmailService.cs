eec2984fca4609ac4df7ad2dc6f2c0fe9e888903
Gerardo Aguilar <455198@petco.com>
2025-01-27 17:44:29 -0600

    Progress on modifying service to send email with attachments. Pending to work with Azure Blob Storage

--- CustomsConsultancy.Admin.Api/Contracts/IEmailService.cs
+++ CustomsConsultancy.Admin.Api/Contracts/IEmailService.cs
@@ -3,5 +3,6 @@ namespace CustomsConsultancy.Admin.Api.Contracts
     public interface IEmailService
     {
         Task Send(string emailToSend, string response, bool isPotentialClient);
+        Task SendWithAttachment(string emailToSend, string response, string attachmentPath, string fileName);
     }
 }
--- CustomsConsultancy.Admin.Api/Services/EmailService.cs
+++ CustomsConsultancy.Admin.Api/Services/EmailService.cs
@@ -1,6 +1,7 @@
 using CustomsConsultancy.Admin.Api.Contracts;
 using MimeKit;
 using MailKit.Net.Smtp;
+using CustomsConsultancy.Admin.Api.Extensions;
 
 namespace CustomsConsultancy.Admin.Api.Services
 {
@@ -8,16 +9,18 @@ namespace CustomsConsultancy.Admin.Api.Services
     {
         public async Task Send(string emailToSend, string response, bool isPotentialClient)
         {
-            var message = new MimeMessage
-            {
-                Subject = isPotentialClient ? $"I.C. Aduanal - Contacto" : "I.C. Aduanal - Respuesta a Pregunta"
-            };
-            message.From.Add(MailboxAddress.Parse("gerraguilar@gmail.com"));
-            message.To.Add(MailboxAddress.Parse(emailToSend));
-            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text = response
-            };
+            var message = CreateMessage(isPotentialClient, response, emailToSend);
+            await SendEmail(message);
+        }
+
+        public async Task SendWithAttachment(string emailToSend, string response, string attachmentPath, string fileName)
+        {
+            var message = CreateMessage(false, response, emailToSend, attachmentPath, fileName);
+            await SendEmail(message);
+        }
+
+        private static async Task SendEmail(MimeMessage message)
+        {
             using var client = new SmtpClient();
             await client.ConnectAsync("smtp.gmail.com", 587, false);
 
@@ -26,6 +29,30 @@ namespace CustomsConsultancy.Admin.Api.Services
             await client.SendAsync(message);
 
             await client.DisconnectAsync(true);
+        }
+
+        private static MimeMessage CreateMessage(bool isPotentialClient, string response, string emailToSend, string attachmentPath = "", string fileName = "")
+        {
+            var message = new MimeMessage
+            {
+                Subject = isPotentialClient ? $"I.C. Aduanal - Contacto" : "I.C. Aduanal - Respuesta a Pregunta"
+            };
+
+            message.From.Add(MailboxAddress.Parse("gerraguilar@gmail.com"));
+            message.To.Add(MailboxAddress.Parse(emailToSend));
+
+            var body = new BodyBuilder
+            {
+                TextBody = response
+            };
+
+            if (attachmentPath.HasValue() && fileName.HasValue())
+            {
+                body.Attachments.Add(fileName, File.ReadAllBytes(attachmentPath));
+            }
+            message.Body = body.ToMessageBody();
+
+            return message;
         }
     }
 }
